20c6df18a97dda0d0eea6601c3dfc311dc3d0d8e
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-09-17 17:37:24 +0700

    lab1

--- node.js/lab1/web.js
+++ node.js/lab1/web.js
@@ -1,14 +1,14 @@
 // khai bao module http de tao web server
 const http = require('http')
-// // khai bao local server
-// const host = 'localhost' // 127.0.0.1
-// // khai bao ser port
-// const port = 3000
+// khai bao local server
+const host = 'localhost' // 127.0.0.1
+// khai bao ser port
+const port = 3000
 // tao web server 
 const server = http.createServer((req, res) => {
     res.end('Hello World. This is my website')
 })
 // chay server
 server.listen(3000, () =>{
-    console.log('Server is running!')
+    console.log('Server is running at http:' + host + ':' + port)
 })
