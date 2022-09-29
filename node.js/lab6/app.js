277067bbe8164a5d35f7675be760097a1e78a3e1
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-09-29 15:00:01 +0700

    lab6

--- node.js/lab6/app.js
+++ node.js/lab6/app.js
@@ -5,7 +5,7 @@ const path = require('path')
 const port = process.env.PORT || 3000;  
 
 //set thư mục chứa views(giao diện - frontend)
-app.set("views",__dirname + '/views')
+app.set("views",__dirname + '/views/hbs')
 //set view engine (template engine)
 app.set("view engine", "hbs")
 
@@ -14,13 +14,23 @@ app.set("view engine", "hbs")
 app.get("/", (req, res) => {
     let name = "Greenwich University";
     let address = "2 Pham Van Bach";
-    res.render("index", { n: name, a: address });
+    let city = "Hanoi";
+    res.render("index", { n: name, a: address, c: city });
 });
 
 app.get("/city", (req, res) => {
     let cities = ["Ha Noi", "HCM city", "Da Nang", "Can Tho"]
-    res.render("city", { country : "VietNam", cities: cities})
+    let provides = ["FPT", "Viettel", "VNPT"]
+    res.render("city", { country : "VietNam", cities: cities, provides: provides})
 })
+
+app.get("/login", (req, res) => {
+    res.render("login");
+  });
+  
+  app.get("/logout", (req, res) => {
+    res.render("logout");
+  });
 
 //chạy web server
 app.listen(port, () => {
